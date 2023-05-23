34bcf6385f4101779592bb79271cd7fc7f5cde6c
Nicolas Rivera <nicolas.rivera@globant.com>
2023-05-23 13:27:44 -0300

    footerPageObj

--- cypress/support/pageObjects/footerPageObj.js
+++ cypress/support/pageObjects/footerPageObj.js
@@ -3,5 +3,27 @@ class footerPageObj {
         return cy.get(':nth-child(1) > .thumbnail > .caption > .grrrr > b')
     }
 
+    aboutUSText() {
+        return cy.get(':nth-child(1) > .thumbnail > .caption > p')
+    }
 
+    getInTouchTitle() {
+        return cy.get('.col-sm-3 > .thumbnail > .caption > .grrrr > b')
+    }
+
+    address() {
+        return cy.get('.col-sm-3 > .thumbnail > .caption > :nth-child(2)')
+    }
+
+    phone() {
+        return cy.get('.caption > :nth-child(3)')
+    }
+
+    email() {
+        return cy.get('.caption > :nth-child(4)')
+    }
+
+    icon() {
+        return cy.get('h4 > img')
+    }
 }
